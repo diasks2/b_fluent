1c4bcd9ffff69bb570591e9b4c053204881cf519
Kevin <diasks2@gmail.com>
2012-04-01 18:01:56 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BFluent::Application.config.secret_token = '056559f9b3ebdcaf04a27e308d164a7a1242f48ae3e68ff3c91dd3783a9d5439718c7fc79608ffda5be017586b1b79165c40beca89d4cd7e3621953695a5ffb2'
